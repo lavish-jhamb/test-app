0464d2e6345bf1c4d5d199501c3f388f54167c9b
Lavish jhamb <lavishjhamb1@gmail.com>
2022-02-10 23:33:14 +0530

    add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
-const Gun = require('Gun')
+const Gun = require('gun')
 
 const PORT = 3000 || process.env.PORT;
 
